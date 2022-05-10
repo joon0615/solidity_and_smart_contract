2e174c6c535cd090a8de1ba19e042c6dfcb10556
Hyunjoon Jung <hyunjoon.jung.99@gmail.com>
2022-05-10 15:06:30 +0900

    feat: write lottery contract

---
+++ project_lottery_v2/contracts/lottery.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.4.0 <0.8.0;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+    address payable public winner;
+    uint public price = 0.1 ether;
+
+    // no need to write visibility(ex. public)
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value >= price);
+        players.push(msg.sender);
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager);
+
+        // pick winner
+        // use payable keyword to change the type
+        // randomized pick by getting residue of random number divided by player list length
+        winner = payable(players[_random() % players.length]);
+
+        // send prize
+        winner.transfer(address(this).balance);
+
+        // initialize players list
+        players = new address[](0);
+    }
+
+    function _random() private view returns (uint) {
+        // get hash based on block difficulty, time stamp, players info
+        // Caution: miners can affect the block info.
+        return uint(keccak256(
+            abi.encodePacked(block.difficulty, block.timestamp, players)
+        ));
+    }
+}
